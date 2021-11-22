7bf7fc62ceb4d6fae0a1a0e57b72aabae5a1c8e6
Yukio Nozawa <personal@nyanchangames.com>
2021-11-22 20:56:14 +0900

    Fix elements

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import React from 'react';
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import { Container } from "react-bootstrap";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Top from "./contents/main/Top";
 import Error from "./contents/error/Error";
 import SharedHeader from "./contents/shared/Header";
 
 function App() {
-    React.useEffect(()=>{
-        let ad = document.body.firstChild;
-        alert("ad element count " + ad.length);
-        for(let i=0;i<ad.length;i++){
-            alert(""+i+": "+ad[i].nodeName);
-        }
-    }, []);
+  React.useEffect(() => {
+    alert(document.body.firstChild);
+    let ad = document.body.firstChild.children;
+    if (ad) {
+      alert("ad element count " + ad.length);
+      for (let i = 0; i < ad.length; i++) {
+        alert("" + i + ": " + ad[i].nodeName);
+      }
+    }
+  }, []);
 
-    return (
-        <Container style={{"max-width": "720px"}}>
-            <SharedHeader />
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/error" component={Error} />
-                    <Route path="/" component={Top} />
-                </Switch>
-            </BrowserRouter>
-        </Container>
-    );
+  return (
+    <Container style={{ "max-width": "720px" }}>
+      <SharedHeader />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/error" component={Error} />
+          <Route path="/" component={Top} />
+        </Switch>
+      </BrowserRouter>
+    </Container>
+  );
 }
 
 export default App;
